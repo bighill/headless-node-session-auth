e9e438296181295488c74ff48907b7c9da439418
lex <alex@bighill.org>
2020-11-23 20:39:33 -0800

    Socket auth via passport

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,27 @@
 import { createServer } from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import app from "./app";
+import session from "./lib/session";
+import passport from "./lib/passport";
 import sockets from "./sockets/sockets";
 
 const server = createServer(app);
 const io = new Server(server);
+const wrap = (middleware: any) => {
+  return (socket: Socket, next: any) => middleware(socket.request, {}, next);
+};
+
+io.use(wrap(session));
+io.use(wrap(passport.initialize()));
+io.use(wrap(passport.session()));
+io.use((socket: any, next) => {
+  if (socket.request.user) {
+    next();
+  } else {
+    next(new Error("unauthorized"));
+  }
+});
+
 io.on("connect", sockets);
 
 server.listen(app.get("port"), () => {
