07ce9d89d51e72358b32ef2db7ead521afba670e
lex <alex@bighill.org>
2020-12-05 17:34:08 -0800

    Put session secret in env var.

--- backend/src/lib/session.ts
+++ backend/src/lib/session.ts
@@ -6,7 +6,7 @@ const RedisStore = connectRedis(session);
 
 const s = session({
   store: new RedisStore({ client: redisdb }),
-  secret: "cdc722f09b746bd818f8b6c4d53796e846137a6b13b703d64a9f60ca619d1fa9",
+  secret: String(process.env.SESSION_SECRET),
   resave: false,
   saveUninitialized: true,
   cookie: {
